170ac6b22d723ed599036358cc66fd1ad2dcd6a6
Chris Regan <cregan22@gmail.com>
2020-05-08 16:16:29 -0700

    added http code text back in as they are not always provided by rgp response

--- src/RGP.php
+++ src/RGP.php
@@ -96,11 +96,14 @@ class RGP {
 
 		// handle any http codes that are not success
 		if ($http_code < 200 || $http_code > 299) {
+			// get standard HTTP code text
+			$error = $this->_http_code_text($http_code);
+
 			// get any error message from rgp
 			$result_array = json_decode($result, true);
 			$msg = $result_array["message"];
 
-			throw new \Exception("RGP Request Error: $http_code - $msg");
+			throw new \Exception("RGP Request Error: $error - $msg");
 		}
 
 		// convert json to array
@@ -161,6 +164,72 @@ class RGP {
 		$path = trim($path);
 		if (substr($path, 0, 1) === "/") return $path;
 		else return "/{$path}";
+	}
+
+
+	// Provide http status code information
+	private function _http_code_text($http_code) {
+		switch($http_code) {
+			case 100: $text = "$http_code - Continue"; break;
+			case 101: $text = "$http_code - Switching Protocols"; break;
+			case 200: $text = "$http_code - OK"; break;
+			case 201: $text = "$http_code - Created"; break;
+			case 202: $text = "$http_code - Accepted"; break;
+			case 203: $text = "$http_code - Non-Authoritative Information"; break;
+			case 204: $text = "$http_code - No Content"; break;
+			case 205: $text = "$http_code - Reset Content"; break;
+			case 206: $text = "$http_code - Partial Content"; break;
+			case 300: $text = "$http_code - Multiple Choices"; break;
+			case 301: $text = "$http_code - Moved Permanently"; break;
+			case 302: $text = "$http_code - Moved Temporarily"; break;
+			case 303: $text = "$http_code - See Other"; break;
+			case 304: $text = "$http_code - Not Modified"; break;
+			case 305: $text = "$http_code - Use Proxy"; break;
+			case 400: $text = "$http_code - Bad Request"; break;
+			case 401: $text = "$http_code - Unauthorized"; break;
+			case 402: $text = "$http_code - Payment Required"; break;
+			case 403: $text = "$http_code - Forbidden"; break;
+			case 404: $text = "$http_code - Not Found"; break;
+			case 405: $text = "$http_code - Method Not Allowed"; break;
+			case 406: $text = "$http_code - Not Acceptable"; break;
+			case 407: $text = "$http_code - Proxy Authentication Required"; break;
+			case 408: $text = "$http_code - Request Time-out"; break;
+			case 409: $text = "$http_code - Conflict"; break;
+			case 410: $text = "$http_code - Gone"; break;
+			case 411: $text = "$http_code - Length Required"; break;
+			case 412: $text = "$http_code - Precondition Failed"; break;
+			case 413: $text = "$http_code - Request Entity Too Large"; break;
+			case 414: $text = "$http_code - Request-URI Too Large"; break;
+			case 415: $text = "$http_code - Unsupported Media Type"; break;
+			case 416: $text = "$http_code - Range Not Satisfiable"; break;
+			case 417: $text = "$http_code - Expectation Failed"; break;
+			case 418: $text = "$http_code - I'm a teapot"; break;
+			case 421: $text = "$http_code - Misdirected Request"; break;
+			case 422: $text = "$http_code - Unprocessable Entity"; break;
+			case 423: $text = "$http_code - Locked"; break;
+			case 424: $text = "$http_code - Failed Dependency"; break;
+			case 425: $text = "$http_code - Too Early"; break;
+			case 426: $text = "$http_code - Upgrade Required"; break;
+			case 428: $text = "$http_code - Precondition Required"; break;
+			case 429: $text = "$http_code - Too Many Requests"; break;
+			case 431: $text = "$http_code - Request Header Fields Too Large"; break;
+			case 451: $text = "$http_code - Unavailable For Legal Reasons"; break;
+			case 500: $text = "$http_code - Internal Server Error"; break;
+			case 501: $text = "$http_code - Not Implemented"; break;
+			case 502: $text = "$http_code - Bad Gateway"; break;
+			case 503: $text = "$http_code - Service Unavailable"; break;
+			case 504: $text = "$http_code - Gateway Time-out"; break;
+			case 505: $text = "$http_code - HTTP Version not supported"; break;
+			case 506: $text = "$http_code - Variant Also Negotiates"; break;
+			case 507: $text = "$http_code - Insufficient Storage"; break;
+			case 508: $text = "$http_code - Loop Detected"; break;
+			case 510: $text = "$http_code - Not Extended"; break;
+			case 511: $text = "$http_code - Network Authentication Required"; break;
+
+			default: $text = $http_code; break;
+		}
+
+		return $text;
 	}
 
 
