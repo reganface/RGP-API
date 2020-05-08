26e9f9fc9af6ca22e6c895edaaefb27a30b0768c
Chris Regan <cregan22@gmail.com>
2020-05-08 16:21:24 -0700

    updated example to use new version of library

--- example/example.php
+++ example/example.php
@@ -16,40 +16,47 @@ try {
 	$rgp = new reganface\RGP\RGP($api_username, $api_key);
 
 	// Get some data.  In this case all of your facilities.
-	$result = $rgp->get_facilities();
+	$result = $rgp->get('/facilities');
 
 	// The data for all endpoints is returned in the "data" key.
 	// Response info is returned in the "response" key.
 	$facilities = $result["data"];
 
-	// Do stuff with the data.
+	// Do stuff with the data.  In this case, list all facilities and their facility code
+	$first_facility = "";
+	echo "Facilities:\n";
 	if (!empty($facilities)) {
-		foreach($facilities as $facility) {
-			echo "{$facility["code"]} - {$facility["name"]}\n";
+		foreach($facilities as $index => $facility) {
+			$first_facility = $first_facility ?: $facility["code"];		// saving the first facility returned for later
+			echo "{$facility["code"]} - {$facility["name"]}\n";			// output list of locations
 		}
 	}
 
-	// For endpoints that return paginated data, the first page is returned with this inital call.
-	// Subsequent pages can be fetched manually with the fetch_page method.
-	// Note: For the sake of brevity, the code below does not check for the existance of a second page before trying to fetch it.
-	$facility_code = "AAA";		// Add your facility code here, or get it from the get_facilites() call above.
-	$start_date = date("Y-m-d H:i:s", strtotime("yesterday"));
-	$end_date = date("Y-m-d 23:59:59", strtotime("yesterday"));
-	$result = $rgp->get_facility_checkins($facility_code, $start_date, $end_date);
+	// get check-ins for the first facility returned above
+	$params = [
+		"startDateTime" => "2019-01-01 00:00:00",
+		"endDateTime" => "2019-01-01 23:59:59",
+		"customerDetails" => true
+		/* "page" => 3	// to get a different page, you'd need to include the page number here */
+	];
+	$result = $rgp->get("/checkins/facility/{$first_facility}", $params);
 
-	// The first page of the result set is stored in "data" as before.
-	$checkins_p1 = $result["data"];
+	// determin what section of check-ins is being displayed
+	$total = $result["pages"]["itemTotal"];
+	$limit = $result["pages"]["itemPage"];
+	$start = (($result["pages"]["pageCurrent"] - 1) * $limit) + 1;
+	$end = min($start + $limit - 1, $total);
 
-	// The pages array contains the url for each page.
-	$page_2_url = $result["pages"][1];
-
-	// Fetch the data from the second page
-	$result_2 = $rgp->fetch_page($page_2_url);
-
-	// The data set in result_2 will be the second page of results
-	$checkins_p2 = $result_2["data"];
+	// list this group of check-ins
+	echo "\n{$start} to {$end} of {$total} Check-Ins\n";
+	if (!empty($result["data"])) {
+		foreach($result["data"] as $checkin) {
+			echo "{$checkin["checkin"]["postDate"]} - {$checkin["customer"]["lastName"]}, {$checkin["customer"]["firstName"]}\n";
+		}
+	}
 
 } catch (Exception $e) {
+	// handle any errors here
 	echo $e->getMessage();
 }
 
